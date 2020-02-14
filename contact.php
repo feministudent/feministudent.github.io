7a0ce152d002947d421a65010ad7e75e7d306d13
feministudent <iamafeministudent@gmail.com>
2020-02-13 20:37:50 -0500

    created logo png files to match new color scheme. also created php file and linked contact form to it (hopefully it works).

---
+++ contact.php
@@ -0,0 +1,13 @@
+<!--form php stuff-->
+<?php
+    // send info from form to email
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $formcontent="From: $name \n Message: $message";
+    $recipient = "iamafeministudent@gmail.com";
+    $subject = "Contact Form";
+    $mailheader = "From: $email \r\n";
+    mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+    echo "Thank You!";
+?>
